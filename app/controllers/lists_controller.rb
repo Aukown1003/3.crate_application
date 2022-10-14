0821db5777aff8953cf8f7d812081f6784fbc072
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-14 11:33:58 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,14 +2,14 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def crate
     # データの受け取り、新規作成するためのインスタンスの作成
-    list = List.new(list_params)  
+    list = List.new(list_params)
     # データの受け取りを保存するためのsaveメソッド
     list.save
     # トップ画面に移動
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -17,12 +17,12 @@ class ListsController < ApplicationController
   end
 
   def show
-    
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
