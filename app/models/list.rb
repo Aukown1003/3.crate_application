7facf81d3b16805b1ea66ae93db82b52afa958c3
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-15 08:04:41 +0000

    [Start]3-8

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,7 @@ class List < ApplicationRecord
   has_one_attached :image
   # １対多（複数枚画像投稿）で関連付ける場合は
   # has_many_attached :images と記載
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
